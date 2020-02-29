d33e974f4a54190ed3fc3b7f0735c9a88cdc58ba
Michael080 <mrollin808@gmail.com>
2020-02-29 15:16:54 -0700

    Answer question 6 regarding error message

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/Exercises/exercises.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/Exercises/exercises.rb
@@ -80,3 +80,15 @@ end
 screamer("Yippeee")
 
 puts "screamer('Yippeee')  =>  nil"
+
+
+#============================================================
+
+#What does the following error message tell you?
+
+# ArgumentError: wrong number of arguments (1 for 2)
+#   from (irb):1:in `calculate_product'
+#   from (irb):4
+#   from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+puts "Answer: The method has two parameters but was called with only one argument"
