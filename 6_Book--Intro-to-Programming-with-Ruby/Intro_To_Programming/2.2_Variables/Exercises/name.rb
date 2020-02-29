4427069331f1c8821a404f0adaf549afa1471998
Michael080 <mrollin808@gmail.com>
2020-02-28 19:00:33 -0700

    Add exercise question as a comment to top of file

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.2_Variables/Exercises/name.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.2_Variables/Exercises/name.rb
@@ -1,3 +1,5 @@
+#Write a program called name.rb that asks the user to type in their name and then prints out a greeting message with their name included.
+
 print "What is your name-o?"
 name = gets.chomp
 puts "Hello #{name} pleasure to meet you."
