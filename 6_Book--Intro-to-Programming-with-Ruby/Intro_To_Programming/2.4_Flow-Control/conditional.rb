8c7b41a672b2795ba05e4d3305ca0d438c27c460
Michael080 <mrollin808@gmail.com>
2020-02-29 16:41:22 -0700

    Add variation of syntax and example of res. word 'unless'

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/conditional.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/conditional.rb
@@ -12,5 +12,10 @@ else
 end
 
 # one-line syntax
-
 if x == 3 then puts "x is 3" end
+
+# if condition @ end
+puts "x is 3" if x == 3
+
+# reserved word unless - the opposite of if
+puts "x is NOT 3" unless x == 3
