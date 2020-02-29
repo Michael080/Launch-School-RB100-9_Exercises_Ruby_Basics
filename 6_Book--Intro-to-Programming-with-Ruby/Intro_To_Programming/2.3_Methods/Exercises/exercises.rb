e1fa1e19265e34312776b8f76432de54ccad62dc
Michael080 <mrollin808@gmail.com>
2020-02-29 14:54:01 -0700

    Answer 1 - 5

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/Exercises/exercises.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/Exercises/exercises.rb
@@ -1,7 +1,31 @@
-#Write a program that prints a greeting message. This program should contain a method called greeting that takes a name as its parameter and returns a string.
+# Write a program that prints a greeting message. This program should contain a method called greeting that takes a name as its parameter and returns a string.
+puts "=================="
+puts "Q1"
+puts "------------------"
 
 def greeting(name)
   puts "Hello, #{name}"
 end
 
 greeting("Zaphod")
+
+# What do the following expressions evaluate to?
+puts "=================="
+puts "Q2"
+puts "------------------"
+# 1. x = 2
+puts "1. x = 2   =>  2"
+
+# 2. puts x = 2 
+puts "2. puts x = 2  =>  nil"
+
+# 3. p name = "Joe"
+puts "3. p name = 'Joe'  =>  'Joe'"
+# NOTE:  p() is a Kernel method which applies inspect to the standard output
+
+# 4. four = "four"
+puts "4. four = 'four'  =>  'four'"
+
+# 5. print something = "nothing"
+puts "5. print something = 'nothing'  =>  nil"
+
