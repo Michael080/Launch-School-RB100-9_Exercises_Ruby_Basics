4811d2dab143212859cacd6a63c4afd95a243ff7
Michael080 <mrollin808@gmail.com>
2020-02-28 20:24:26 -0700

    Add loop that prints 'name' 10 times per instructions for ex. #4

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.2_Variables/Exercises/name.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.2_Variables/Exercises/name.rb
@@ -3,3 +3,12 @@
 print "What is your name-o?"
 name = gets.chomp
 puts "Hello #{name} pleasure to meet you."
+
+=begin 
+Additional exercise:
+Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row. Hint: you can use the times method to do something repeatedly. 
+=end
+
+10.times do |n|
+  puts "#{name}"
+end
