26ca06da4f6e0ac9ef20d021484f152f79a2e411
Michael080 <mrollin808@gmail.com>
2020-02-29 13:34:57 -0700

    Add example of the samemethod with a return keyword

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/return.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/return.rb
@@ -1,1 +1,17 @@
-git a
+def add_three(number)
+  number += 3
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+#same code but with a return keyword in the method:
+puts "===================="
+
+def add_threee(number)
+  return number += 3
+  #this method would return the value regarless of whether the return keyword is present or not
+end
+
+puts add_threee(4)
+
