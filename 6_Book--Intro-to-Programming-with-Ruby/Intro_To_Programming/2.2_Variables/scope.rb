08cb2188f51dd26ba0f55e7028ee11a6b5d64d65
Michael080 <mrollin808@gmail.com>
2020-02-28 18:15:19 -0700

    Add var b to block

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.2_Variables/scope.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.2_Variables/scope.rb
@@ -5,8 +5,12 @@ a = 5           # variable is initialized in the outer scope
 3.times do |n|  # method invocation with a block
   puts "a w/in block: #{a}"
   a = 3         # is a accessible here, in an inner scope?
+  # b = 5         # b is initialized in the inner scope
 end
 
 puts "===================================="
 puts "a after reassignment in block: #{a}"
+puts "===================================="
+puts "b from outer scope after assignment in block: #{b}"
+# Produces 'undefined local variable'
   
