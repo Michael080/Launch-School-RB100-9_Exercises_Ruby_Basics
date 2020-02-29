9daaf6fdfe5681c87686faa323fdb4f1cd47f17e
Michael080 <mrollin808@gmail.com>
2020-02-29 14:28:24 -0700

    Add method 'multiply' and call it with methods as arguments

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/method_calls_as_args.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/method_calls_as_args.rb
@@ -9,3 +9,15 @@ end
 puts add(20, 45)
 
 puts subtract(80, 10)
+
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+#call multiply w/ add and subtract as arguments
+puts multiply(add(20, 45), subtract(80, 10))
+
+# a more complex example
+puts add(subtract(80, 10), multiply(add(20, 45), subtract(80, 10)))
+
