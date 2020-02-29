89f8f459e3d67ecfb70f0de494957ac0fa99a350
Michael080 <mrollin808@gmail.com>
2020-02-29 13:58:01 -0700

    Commented out definition and call of add_five method so that errors don't interrupt running the code examples that follow

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/chaining_methods.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.3_Methods/chaining_methods.rb
@@ -10,10 +10,15 @@ add_four(4).times { puts 'this should print 8 times'}
 
 
 # basically the same code but with a puts expression
-puts "====================="
 
-def add_five(n) 
-  puts n + 5
-end
+# puts "====================="
 
-add_five(8).times { puts 'will this work?'}
+# def add_five(n) 
+#   puts n + 5
+# end
+
+# add_five(8).times { puts 'will this work?'}
+
+#this method call produces error:
+#'undefined method 'times' for nil:NilClass
+#this is because 'puts n + 5' is a nil value
