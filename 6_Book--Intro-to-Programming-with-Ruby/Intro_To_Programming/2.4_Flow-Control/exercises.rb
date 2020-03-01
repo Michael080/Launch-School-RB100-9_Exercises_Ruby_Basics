ff6ba7040a0edb91c0cd936e0f5ce460418a1ec6
Michael080 <mrollin808@gmail.com>
2020-03-01 16:59:16 -0700

    Evaluate boolean expressions of Snippets 1 - 3

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/exercises.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/exercises.rb
@@ -54,3 +54,52 @@ end
 
 puts find_range(user_number)
 
+
+# What will each block of code below print to the screen? Write your answer on a piece of paper or in a text editor and then run each block of code to see if you were correct.
+puts "======================
+4."
+
+# Snippet 1
+'4' == 4 ? puts("TRUE") : puts("FALSE")
+
+#ANSWER
+puts("--------------
+  Snippet #1  Answer")
+puts "FALSE"
+puts "--------------"
+
+
+# Snippet 2
+x = 2
+if ((x * 3) / 2) == (4 + 4 - x - 3)
+  puts "Did you get it right?"
+else
+  puts "Did you?"
+end
+
+#ANSWER
+puts("--------------
+  Snippet #2  Answer")
+puts "Dig you get it right?"
+puts "--------------"
+
+
+# Snippet 3
+y = 9
+x = 10
+if (x + 1) <= (y)
+  puts "Alright."
+elsif (x + 1) >= (y)
+  puts "Alright now!"
+elsif (y + 1) == x
+  puts "ALRIGHT NOW!"
+else
+  puts "Alrighty!"
+end
+
+#ANSWER
+puts("--------------
+  Snippet #3  Answer")
+puts "Alright now!"
+puts "--------------"
+
