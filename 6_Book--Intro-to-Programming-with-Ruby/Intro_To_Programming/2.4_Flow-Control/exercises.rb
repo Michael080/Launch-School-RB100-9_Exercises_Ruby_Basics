c99eb5ae5c5682d4be99e0fd41eb02e9fcb55b38
Michael080 <mrollin808@gmail.com>
2020-03-01 16:21:02 -0700

    Create cap_ten() method which converts 10 character or less strings to uppercase

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/exercises.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/exercises.rb
@@ -1,4 +1,6 @@
 # Write down whether the following expressions return true or false. Then type the expressions into irb to see the results.
+puts "======================
+1."
 
 (32 * 4) >= 129
 puts "false"
@@ -14,3 +16,19 @@ puts "true"
 
 (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false
 puts "true"
+
+
+# Write a method that takes a string as argument. The method should return a new, all-caps version of the string, only if the string is longer than 10 characters. Example: change "hello world" to "HELLO WORLD". (Hint: Ruby's String class has a few methods that would be helpful. Check the Ruby Docs!)
+puts "======================
+2."
+
+def cap_ten(string)
+  if string.length > 10
+    puts "This string is too long"
+  else
+    string.upcase
+  end
+end
+
+puts cap_ten("blammo")
+puts cap_ten("Incredibly very too long string")
