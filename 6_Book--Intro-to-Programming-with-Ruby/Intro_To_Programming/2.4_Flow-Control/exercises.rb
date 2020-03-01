116f59e41072f11441a916008c76469456639343
Michael080 <mrollin808@gmail.com>
2020-03-01 15:33:30 -0700

    Evaluate five boolean expressions

---
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/exercises.rb
@@ -0,0 +1,16 @@
+# Write down whether the following expressions return true or false. Then type the expressions into irb to see the results.
+
+(32 * 4) >= 129
+puts "false"
+
+false != !true
+puts "false"
+
+true == 4
+puts "false"
+
+false == (847 == '874')
+puts "true"
+
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false
+puts "true"
