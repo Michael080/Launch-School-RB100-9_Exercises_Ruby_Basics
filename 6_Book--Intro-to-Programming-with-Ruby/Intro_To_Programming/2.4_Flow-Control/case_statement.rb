d424312b7268b483284d15616802c960014f7e75
Michael080 <mrollin808@gmail.com>
2020-03-01 15:13:12 -0700

    Add example of case statement w/out an arg.

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/case_statement.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/case_statement.rb
@@ -26,3 +26,18 @@ else
 end
 
 puts answer
+
+
+# case with no arg
+
+a = 5
+
+case  
+when a == 5
+  #each case needs to be 'fully-tested' w/ this approach
+  puts "a is 5"
+when a == 6
+  puts "a is 6"
+else
+  puts "a is neither 5, nor 6"
+end
