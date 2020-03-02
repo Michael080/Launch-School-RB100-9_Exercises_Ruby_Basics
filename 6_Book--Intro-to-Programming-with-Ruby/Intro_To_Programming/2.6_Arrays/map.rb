b237349cd43ea8e397ae62f344eb7c28d891fd92
Michael080 <mrollin808@gmail.com>
2020-03-02 12:59:27 -0700

    Add ex. of a.map returning Enumerator

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.6_Arrays/map.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.6_Arrays/map.rb
@@ -4,6 +4,14 @@ puts b
 
 #example showing map returning new array
 puts "------------------"
+
 a.map { |x| puts x**2 }
 puts "returns: 1, 4, 9
 => [nil, nil, nil]"
+
+#if no block is given, map returns Enumerator:
+puts "------------------"
+
+a.map
+puts "returns:
+#<Enumerator: [1, 2, 3]:map>"
