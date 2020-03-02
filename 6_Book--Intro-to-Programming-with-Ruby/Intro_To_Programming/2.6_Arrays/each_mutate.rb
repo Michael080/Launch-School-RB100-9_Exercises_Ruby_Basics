0b1d6dfc6e04bf88fb2ab21a405a2ae0201ec822
Michael080 <mrollin808@gmail.com>
2020-03-02 12:47:16 -0700

    Create exs. of .each iterating over an array & .each w/ no block returning an Enumerator

---
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.6_Arrays/each_mutate.rb
@@ -0,0 +1,17 @@
+#modify result of .each method
+
+a = [1, 2, 3]
+a.each { |e| puts e + 2}
+
+puts "----------------"
+puts "result: 3, 4, 5"
+
+
+# .each w/ no block and return Enumerator:
+puts "================="
+
+a.each
+
+
+
+
