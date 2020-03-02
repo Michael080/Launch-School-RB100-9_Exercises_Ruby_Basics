2f395a001ca9946135f47710b065ec6c6a3f0901
Michael080 <mrollin808@gmail.com>
2020-03-02 13:32:07 -0700

    Evaluate methods applied to array and determine output

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.6_Arrays/Exercises/exercises.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.6_Arrays/Exercises/exercises.rb
@@ -6,3 +6,25 @@ arr = [1, 3, 5, 7, 9, 11]
 number = 3
 
 puts arr.include?(number)
+
+
+# What will the following programs return? What
+# is the value of arr after each?
+puts "======================="
+
+a = ["b", "a"]
+a = a.product(Array(1..3))
+puts a.first.delete(a.first.last)
+
+puts "returns:
+['b', 1], ['b', 2], ['b', 3] etc,."
+puts "&..."
+puts "removes:
+'1' of ['b', 1]"
+
+puts "------------------------"
+
+# b = ["b", "a"]
+# arr = arr.product([Array(1..3)])
+
+# arr.first.delete(arr.first.last)
