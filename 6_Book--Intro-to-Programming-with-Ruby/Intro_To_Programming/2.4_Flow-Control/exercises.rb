22511fe830b57bf6c964f2253fa662a37b2ee140
Michael080 <mrollin808@gmail.com>
2020-03-01 17:08:19 -0700

    Fix bug in equal_to_four() method

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/exercises.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.4_Flow-Control/exercises.rb
@@ -103,3 +103,34 @@ puts("--------------
 puts "Alright now!"
 puts "--------------"
 
+
+# When you run the following code...
+puts "======================
+4."
+
+# def equal_to_four(x)
+#   if x == 4
+#     puts "yup"
+#   else
+#     puts "nope"
+# end
+
+# equal_to_four(5)
+
+# You get the following error message..
+
+# exercise.rb:8: syntax error, unexpected end-of-input, expecting keyword_end
+
+# Why do you get this error and how can you fix it?
+puts "The end keyword is missing from the if statement.
+Add end statement after 'else'"
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+  end
+end
+
+equal_to_four(5)
