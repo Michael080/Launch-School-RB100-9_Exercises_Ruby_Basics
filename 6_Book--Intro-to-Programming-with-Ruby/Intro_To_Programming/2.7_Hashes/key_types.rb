43d83d0711726782074fea5ea65b6a9cea5dac28
Michael080 <mrollin808@gmail.com>
2020-03-03 21:43:30 -0700

    Create ex. of different data types that can be used as keys in a hash

---
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.7_Hashes/key_types.rb
@@ -0,0 +1,17 @@
+# Strings, arrays, integers, floats, and hashes can be used as keys
+# But, the 'old style' syntax must be used:
+
+#STRING
+{"height" => "6 ft"}
+
+#ARRAY
+{["height"] => "6 ft"}
+
+#INTEGER
+{1 => "one"}
+
+#FLOAT
+{45.324 => "forty-five point something"}
+
+#HASH
+{{key: "key"} => "hash as a key"}
