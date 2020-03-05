951f7b754c467012a150817bd41599a4bc923030
Michael080 <mrollin808@gmail.com>
2020-03-04 23:00:58 -0700

    Create 2 hashes and show examples of performing .merge() & .merge!() on them

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.7_Hashes/Exercises/exercises.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.7_Hashes/Exercises/exercises.rb
@@ -2,6 +2,8 @@
 # and an array of names as the values, use Ruby's built-in
 # select method to gather only immediate family members'
 # names into a new array.
+puts "-----------------------------------------------------
+1.)"
 
 family = {  uncles: ["bob", "joe", "steve"],
             sisters: ["jane", "jill", "beth"],
@@ -21,4 +23,29 @@ atomic_fam.first.shift
 atomic_fam.last.shift
 # Remove nesting (flatten)
 atomic_fam.flatten!
+
+
+# Look at Ruby's merge method. Notice that it has two
+# versions. What is the difference between merge and merge!?
+# Write a program that uses both and illustrate the
+# differences.
+puts "-----------------------------------------------------
+2.)"
+
+puts "* merge -  Returns a new hash of the combined contents of the
+receiver and given hashes. Duplicates are overriddden where the given
+hash takes precedence"
+puts "* merge! -  Returns a new hash of the combined contents of the
+receiver and given hashes. Duplicates are overriddden where the given
+hash takes precedence."
+
+mighty_hash = {bestness: 1, superiority: 0.85, mightiness: "Ya' baby!"}
+
+hashy_hash = {bestness: 0.8, superiority: 0.35, hashiness: 100}
+
+# merge
+mighty_hash.merge(hashy_hash)
+
+# merge!
+mighty_hash.merge!(hashy_hash)
 
