bcebb6c7c3220b2080021d217cbc9d007c2d0be8
Michael080 <mrollin808@gmail.com>
2020-03-13 00:16:10 -0600

    Make var names more descriptive

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.7_Hashes/Exercises/ex_8.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.7_Hashes/Exercises/ex_8.rb
@@ -4,23 +4,23 @@ words = ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
 
 word_1 = words[4]
 word_2 = words[5]
-word_1_split = words[4].split("")
-word_2_split_split = words[5].split("")
+word_1_temp = words[4].split("")
+word_2_temp = words[5].split("")
 array = []
 anagrams = []
 template_length = word_2.length
 $iterator = 0
 
-word_1_split.each do |char|
+
+
+word_1_temp.each do |char|
   sample = char
-
-  word_2_split.each do |char|
-
+  word_2_temp.each do |char|
     puts "#{sample} : #{char}"
     if (sample == char)
       array.push(char)
       puts "$iterator:#{$iterator}  char:#{char}"
-      word_2_split.delete_at($iterator)
+      word_2_temp.delete_at($iterator)
       $iterator += 1
     else
       $iterator += 1
