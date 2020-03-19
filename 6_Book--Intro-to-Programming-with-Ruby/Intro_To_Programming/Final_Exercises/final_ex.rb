10faf00f63b8366a024368ab24788319e99ab3be
Michael080 <mrollin808@gmail.com>
2020-03-19 11:12:08 -0600

    Iterate over array and print items greater than 5

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/Final_Exercises/final_ex.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/Final_Exercises/final_ex.rb
@@ -9,3 +9,13 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numbers.each do |num|
   puts num
 end
+
+
+##############################################################
+puts "--------------------------------------------------------
+2.)"
+# Same as above, but only print out values greater than 5.
+
+numbers.each do |num|
+  num > 5 ? (puts num) : next
+end
