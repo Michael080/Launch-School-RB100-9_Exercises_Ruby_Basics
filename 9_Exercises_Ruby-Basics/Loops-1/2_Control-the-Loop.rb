8a642cadb148bbe551891a7bc0cf85724f2236a1
Michael080 <mrollin808@gmail.com>
2020-03-20 20:56:07 -0600

    Add Launch School's solution

--- 9_Exercises_Ruby-Basics/Loops-1/2_Control-the-Loop.rb
+++ 9_Exercises_Ruby-Basics/Loops-1/2_Control-the-Loop.rb
@@ -9,3 +9,13 @@ loop do
     break
   end
 end
+
+# Solution from Launch School:
+
+iterations = 1
+
+loop do
+  puts "Number of iterations = #{iterations}"
+  iterations += 1
+  break if iterations > 5
+end
