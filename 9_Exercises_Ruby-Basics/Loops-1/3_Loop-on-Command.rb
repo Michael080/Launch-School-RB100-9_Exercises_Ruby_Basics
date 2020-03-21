7fe7f97a6314bdfe8f24dd872435dff542f06869
Michael080 <mrollin808@gmail.com>
2020-03-20 20:59:30 -0600

    Add LS solution

--- 9_Exercises_Ruby-Basics/Loops-1/3_Loop-on-Command.rb
+++ 9_Exercises_Ruby-Basics/Loops-1/3_Loop-on-Command.rb
@@ -1,5 +1,15 @@
+# Modify the code below so the loop stops iterating
+# when the user inputs 'yes'.
 loop do
   puts 'Should I stop looping?'
   answer = gets.chomp
   break if answer == 'yes'
+end
+
+# Launch Schools solution:
+loop do
+  puts 'Should I stop looping?'
+  answer = gets.chomp
+  break if answer == 'yes'
+  puts 'Incorrect answer. Please answer "yes".'
 end
