3ae4f1ce8b69c55c2357cb800a6d2aa8212e7443
Michael080 <mrollin808@gmail.com>
2020-03-22 16:00:48 -0600

    LS solution

--- 9_Exercises_Ruby-Basics/Loops-2/2_Catch-the-Number.rb
+++ 9_Exercises_Ruby-Basics/Loops-2/2_Catch-the-Number.rb
@@ -5,3 +5,14 @@ loop do
   number = rand(100)
   number.between?(0,10) ? break : (puts number)
 end
+
+
+# LS solution:
+loop do
+  number = rand(100)
+  puts number
+
+  if number.between?(0, 10)
+    break
+  end
+end
