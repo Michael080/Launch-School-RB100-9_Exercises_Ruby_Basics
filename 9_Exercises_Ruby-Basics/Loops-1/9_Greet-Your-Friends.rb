4058f8b69bbe3b52b118936a17fc7cf0e6dfed4f
Michael080 <mrollin808@gmail.com>
2020-03-21 22:28:22 -0600

    LS solution

--- 9_Exercises_Ruby-Basics/Loops-1/9_Greet-Your-Friends.rb
+++ 9_Exercises_Ruby-Basics/Loops-1/9_Greet-Your-Friends.rb
@@ -6,3 +6,10 @@ friends = ['Sarah', 'John', 'Hannah', 'Dave']
 for i in 0..(friends.length - 1)
   puts "Greetings my beloved #{friends[i]}"
 end
+
+# LS solution:
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for friend in friends
+  puts "Hello, #{friend}!"
+end
