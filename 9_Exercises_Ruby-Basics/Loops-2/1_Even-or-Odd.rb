64b2a1ad51a5adb6fdbe8bbe5b1d2a3c644f7ba3
Michael080 <mrollin808@gmail.com>
2020-03-22 14:37:18 -0600

    LS solution

--- 9_Exercises_Ruby-Basics/Loops-2/1_Even-or-Odd.rb
+++ 9_Exercises_Ruby-Basics/Loops-2/1_Even-or-Odd.rb
@@ -15,3 +15,18 @@ loop do
   end
   break
 end
+
+
+# LS solution:
+count = 1
+
+loop do
+  if count.even?
+    puts "#{count} is even!"
+  else
+    puts "#{count} is odd!"
+  end
+
+  break if count == 5
+  count += 1
+end
