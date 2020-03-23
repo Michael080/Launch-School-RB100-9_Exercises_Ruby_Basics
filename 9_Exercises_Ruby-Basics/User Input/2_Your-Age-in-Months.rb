5c11efb05d3483e0d12e370b6874198e7152cd52
Michael080 <mrollin808@gmail.com>
2020-03-23 11:57:47 -0600

    LS solution

--- 9_Exercises_Ruby-Basics/User Input/2_Your-Age-in-Months.rb
+++ 9_Exercises_Ruby-Basics/User Input/2_Your-Age-in-Months.rb
@@ -5,8 +5,14 @@ def age_in_months(age)
   age.to_i * 12
 end
 
-puts "What is your age in year?"
+puts "What is your age in years?"
 age = gets.chomp
 
 puts "Your #{age_in_months(age)} months old!"
 
+
+# LS solution:
+puts '>> What is your age in years?'
+age_in_years = gets
+age_in_months = 12 * age_in_years.to_i
+puts "You are #{age_in_months} months old."
