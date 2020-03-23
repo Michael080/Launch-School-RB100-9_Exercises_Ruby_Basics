5de3ec4dcfb1b2081c4513c8404d445546688b69
Michael080 <mrollin808@gmail.com>
2020-03-23 11:04:25 -0600

    Use loop to remove each element of array

---
+++ 9_Exercises_Ruby-Basics/Loops-2/5_Empty-the-Array.rb
@@ -0,0 +1,14 @@
+# Given the array below, use loop to remove and print each name.
+# Stop the loop once names doesn't contain any more elements.
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+id = 0
+start_size = names.size
+
+loop do
+  puts names[id]
+  names.shift
+  id += 1
+  id == start_size ? break : next
+end
+
