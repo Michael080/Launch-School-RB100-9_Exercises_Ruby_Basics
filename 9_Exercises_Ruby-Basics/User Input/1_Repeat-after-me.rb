727e6261fb3a7119b1d602280d24676f46ae68c2
Michael080 <mrollin808@gmail.com>
2020-03-23 11:45:20 -0600

    Print user input to screen

---
+++ 9_Exercises_Ruby-Basics/User Input/1_Repeat-after-me.rb
@@ -0,0 +1,9 @@
+# Write a program that asks the user to type something in,
+# after which your program should simply display what was
+# entered.
+
+puts "Tell me something..."
+
+repeater = gets.chomp
+
+puts repeater
