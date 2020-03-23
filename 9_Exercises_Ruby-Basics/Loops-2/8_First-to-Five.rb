1038a27f337fa0b3912bd89bf6e13145445a5ea7
Michael080 <mrollin808@gmail.com>
2020-03-23 11:27:06 -0600

    LS solutiond

--- 9_Exercises_Ruby-Basics/Loops-2/8_First-to-Five.rb
+++ 9_Exercises_Ruby-Basics/Loops-2/8_First-to-Five.rb
@@ -16,3 +16,17 @@ loop do
     break
   end
 end
+
+
+# LS solution:
+number_a = 0
+number_b = 0
+
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
+
+  next unless number_a == 5 || number_b == 5
+  puts '5 was reached!'
+  break
+end
