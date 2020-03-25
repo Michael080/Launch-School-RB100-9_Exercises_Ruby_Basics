819cd0fd90cac8813b06ad54e14a3fd02ca192ea
Michael080 <mrollin808@gmail.com>
2020-03-25 15:03:34 -0600

    Evaluate code and determine output

--- 9_Exercises_Ruby-Basics/Return/1_Breakfast-Lunch-or-Dinner.rb
+++ 9_Exercises_Ruby-Basics/Return/1_Breakfast-Lunch-or-Dinner.rb
@@ -9,5 +9,21 @@ def meal
 end
 
 puts meal
-
 puts "Returns: 'Breakfast'"
+
+
+#==============================================================
+# BREAKFAST, LUNCH, OR DINNER (PART 2)
+
+#What will the following code print? Why? Don't run it until
+# you've attempted to answer.
+
+def meal
+  'Evening'
+end
+
+puts meal
+puts "Returns: 'Evening'"
+
+
+
