30aa5a25fec32090d6c8fabbd4c6403bf63b9f9a
Michael080 <mrollin808@gmail.com>
2020-03-27 10:25:55 -0600

    Change string to properly capitalized word

---
+++ 9_Exercises_Ruby-Basics/Strings/6_Tricky-Formatting.rb
@@ -0,0 +1,11 @@
+# Using the following code, capitalize the value of state then print
+# the modified value. Note that state should have the modified value
+# both before and after you print it.
+
+state = 'tExAs'
+
+state = state.downcase
+
+state[0] = state[0].upcase
+
+puts state
