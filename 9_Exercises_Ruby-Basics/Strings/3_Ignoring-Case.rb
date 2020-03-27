0b82af3912d4db81ca0b9f2dc999a727194bd510
Michael080 <mrollin808@gmail.com>
2020-03-26 20:16:00 -0600

    Ls solution

--- 9_Exercises_Ruby-Basics/Strings/3_Ignoring-Case.rb
+++ 9_Exercises_Ruby-Basics/Strings/3_Ignoring-Case.rb
@@ -8,3 +8,10 @@
 name = 'Roger'
 puts name.downcase == 'RoGeR'.downcase
 puts name.downcase == 'DAVE'.downcase
+
+
+# LS solution:
+name = 'Roger'
+
+puts name.casecmp('RoGeR') == 0
+puts name.casecmp('DAVE') == 0
