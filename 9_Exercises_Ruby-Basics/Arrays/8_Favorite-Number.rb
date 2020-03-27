bd268e84a70d5a07652987be8f6cd9664450cb87
Michael080 <mrollin808@gmail.com>
2020-03-27 16:38:14 -0600

    Flatten nested array

--- 9_Exercises_Ruby-Basics/Arrays/8_Favorite-Number.rb
+++ 9_Exercises_Ruby-Basics/Arrays/8_Favorite-Number.rb
@@ -1,3 +1,6 @@
+# =======================================================
+# Favorite Number (Part 1)
+#
 # The following array contains three names and numbers.
 # Group each name with the number following it by
 # placing the pair in their own array. Then create a
@@ -7,3 +10,21 @@
 # requirements.)
 
 [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
+
+# =======================================================
+# Favorite Number (Part 2)
+#
+# In the code below, a nested array containing three groups
+# of names and numbers is assigned to favorites.
+
+# [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
+# Use Array#flatten to flatten favorites so that it's no
+# longer a nested array. Then assign the flattened array to
+# a variable named flat_favorites and print its value using
+# #p.
+
+flat_favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]].flatten
+
+p flat_favorites
