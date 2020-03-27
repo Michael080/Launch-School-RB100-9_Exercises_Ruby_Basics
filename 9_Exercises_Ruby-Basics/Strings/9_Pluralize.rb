6c417e406a8abb3e07fd14a3d157777dbf28e343
Michael080 <mrollin808@gmail.com>
2020-03-27 11:07:40 -0600

    LS solution

--- 9_Exercises_Ruby-Basics/Strings/9_Pluralize.rb
+++ 9_Exercises_Ruby-Basics/Strings/9_Pluralize.rb
@@ -7,3 +7,11 @@ words_arr = words.split(" ")
 words_arr.each do |word|
   puts word + "s"
 end
+
+
+# LS solution:
+words = 'car human elephant airplane'
+
+words.split(' ').each do |word|
+  puts word + 's'
+end
