fea70e4c5fa8500dfdde72b54ecd2f5bd4cf294c
Michael080 <mrollin808@gmail.com>
2020-03-28 17:08:51 -0600

    LS solution

--- 9_Exercises_Ruby-Basics/Debugging/6_Confucius-says.rb
+++ 9_Exercises_Ruby-Basics/Debugging/6_Confucius-says.rb
@@ -21,3 +21,14 @@ end
 puts 'Confucius says:'
 puts '"' + get_quote('Confucius') + '"'
 
+
+# LS solution:
+def get_quote_ls(person)
+  if person == 'Yoda'
+    'Do. Or do not. There is no try.'
+  elsif person == 'Confucius'
+    'I hear and I forget. I see and I remember. I do and I understand.'
+  elsif person == 'Einstein'
+    'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
+  end
+end
