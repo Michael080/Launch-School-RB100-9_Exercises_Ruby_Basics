aab447033342148dc3e66381f0124d8805858bea
Michael080 <mrollin808@gmail.com>
2020-03-28 10:40:32 -0600

    Create hash of car info

---
+++ 9_Exercises_Ruby-Basics/Hashes/1_First-Car.rb
@@ -0,0 +1,14 @@
+# Create a hash that contains the following data and assign
+# it to a variable named car.
+
+# type	color	mileage
+# sedan	blue	80_000
+
+car = {type: 'sedan', color: 'blue', mileage: 80000}
+puts car
+
+# or
+
+car = {:type=>'sedan', :color=>'blue', :mileage=> 80000}
+puts car
+
