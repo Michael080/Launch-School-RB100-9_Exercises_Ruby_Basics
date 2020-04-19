414a152c2dc5224070742dd0d18930e45666fd20
Michael080 <mrollin808@gmail.com>
2020-04-19 13:21:46 -0600

    Create example of modified take_proc

--- 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.8_More-Stuff/passing_proc.rb
+++ 6_Book--Intro-to-Programming-with-Ruby/Intro_To_Programming/2.8_More-Stuff/passing_proc.rb
@@ -9,3 +9,19 @@ proc = Proc.new do |number|
 end
 
 take_proc(proc)
+
+# Procs can be passed into methods. Let's take our previous
+# passing_block.rb code and modify the method to take a proc
+# instead. We'll add some more functionality too.
+
+def take_proc(proc)
+  [1, 2, 3, 4, 5].each do |number|
+    proc.call number
+  end
+end
+
+proc = Proc.new do |number|
+  puts "#{number}. Proc being called in the method!"
+end
+
+take_proc(proc)
