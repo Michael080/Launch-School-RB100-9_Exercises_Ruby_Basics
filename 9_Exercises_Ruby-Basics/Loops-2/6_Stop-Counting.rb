9128f437795c23dbda1a65a5cb1f5d051104620a
Michael080 <mrollin808@gmail.com>
2020-05-03 11:59:05 -0600

    Further exploration

--- 9_Exercises_Ruby-Basics/Loops-2/6_Stop-Counting.rb
+++ 9_Exercises_Ruby-Basics/Loops-2/6_Stop-Counting.rb
@@ -2,14 +2,34 @@
 # prints the current number and stops iterating when the current
 # number equals 2.
 
-5.times do |index|
-  puts index
-  index == 2 ? break : next
-end
+# 5.times do |index|
+#   puts index
+#   index == 2 ? break : next
+# end
+#
+#
+# # LS solution:
+# 5.times do |index|
+#   puts index
+#   break if index == 2
+# end
 
 
-# LS solution:
+# Further Exploration
+# Without running this code, how many values will be printed?
 5.times do |index|
   puts index
-  break if index == 2
+  break if index == 4
 end
+puts "-------------"
+puts "Answer: 5"
+puts "-------------"
+
+# Now, consider this code:
+5.times do |index|
+  puts index
+  break if index < 7
+end
+puts "-------------"
+puts "Answer: 1"
+puts "-------------"
