616e875e4f4a8a067d0f5a553c268382cbd43362
Michael080 <mrollin808@gmail.com>
2020-05-03 12:03:06 -0600

    Further exploration

--- 9_Exercises_Ruby-Basics/Loops-2/7_Only-Even.rb
+++ 9_Exercises_Ruby-Basics/Loops-2/7_Only-Even.rb
@@ -16,3 +16,9 @@ until number == 10
   next if number.odd?
   puts number
 end
+
+# Further Exploration
+# Why did next have to be placed after the incrementation of number and
+# before #puts?
+puts "Answer: In order to skip to the next iteration of the loop if 'number is odd'.
+This allows for printing of 'number' only when 'number' is not odd, or even."
