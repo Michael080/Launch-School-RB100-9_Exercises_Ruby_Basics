af3e1708ba811d1dea3d0e851592f02a5aa27f27
Michael080 <mrollin808@gmail.com>
2020-05-03 19:19:59 -0600

    Further exploration

--- 9_Exercises_Ruby-Basics/Strings/2_Quote-Confusion.rb
+++ 9_Exercises_Ruby-Basics/Strings/2_Quote-Confusion.rb
@@ -1,8 +1,26 @@
 # Modify the following code so that double-quotes are used
 # instead of single-quotes.
-
+puts "----------
+myAnswer"
 # puts 'It\'s now 12 o\'clock.'
 puts "It's now 12 o'clock."
 
 # LS Discussion (alternatives):
 %Q(It's now 12 o'clock.) # => "It's now 12 o'clock."
+
+
+#Further Exploration
+# It's possible to write a string without surrounding it with
+# double- or single-quotes. Instead of using double-quotes,
+# we could write the example string like this
+%Q(It's now 12 o'clock.) # => "It's now 12 o'clock."
+# %Q is an alternative to double-quoted strings. There's also
+# an alternative for single-quoted strings: %q.
+#
+# What about strings that contain double- and single-quotes?
+# How would you write those using the alternate syntax?
+puts "----------
+further exploration:"
+
+puts %Q(here and) + %q{ here}
+
