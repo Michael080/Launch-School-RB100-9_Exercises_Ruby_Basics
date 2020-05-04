9a68e85c46694397e56da9e83dbf1e61d10c53de
Michael080 <mrollin808@gmail.com>
2020-05-04 14:19:54 -0600

    Further exploration

--- 9_Exercises_Ruby-Basics/Hashes/8_Low-or-Nothing.rb
+++ 9_Exercises_Ruby-Basics/Hashes/8_Low-or-Nothing.rb
@@ -15,3 +15,18 @@ end
 
 p low_numbers
 p numbers
+
+
+# Further Exploration
+# In our solution, we assign the return value of #select!
+# to low_numbers. Is this assignment really needed if we
+# mutated numbers? Typically, when using methods that
+# mutate the caller, the return value is of no interest
+# because the caller is intentionally being mutated. If
+# you do an assignment like this, you end up creating an
+# alias: both numbers and low_numbers now point to the
+# same Hash.
+
+puts "Answer: No. If we are mutating the caller the variable
+assignment seems to be redundant. Maybe there are circumstances
+where one would need to do this though."
