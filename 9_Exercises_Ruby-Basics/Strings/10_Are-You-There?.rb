55e23aa3b15545565a6e087e406c2d56d8a7a942
Michael080 <mrollin808@gmail.com>
2020-05-04 14:08:42 -0600

    Further exploration

--- 9_Exercises_Ruby-Basics/Strings/10_Are-You-There?.rb
+++ 9_Exercises_Ruby-Basics/Strings/10_Are-You-There?.rb
@@ -2,7 +2,8 @@
 # the color 'yellow' and print false if it doesn't. Then,
 # print true if colors includes the color 'purple' and
 # print false if it doesn't.
-
+puts "---------
+myAnswer"
 colors = 'blue pink yellow orange'
 
 /yellow/.match(colors) ? puts("true") : puts("false")
@@ -10,7 +11,23 @@ colors = 'blue pink yellow orange'
 
 
 # LS solution:
+puts "---------
+ls solution:"
 colors = 'blue pink yellow orange'
 
 puts colors.include?('yellow')
 puts colors.include?('purple')
+
+
+# Further Exploration
+# Let's say colors is changed to the following value:
+#
+puts "---------
+further exploration"
+colors = 'blue boredom yellow'
+# and we invoke #include? as we did before:
+#
+puts colors.include?('red')
+# What will the output be? Why?
+puts "Answer: The output will be 'true' because 'red'
+is in the word 'boredom'"
