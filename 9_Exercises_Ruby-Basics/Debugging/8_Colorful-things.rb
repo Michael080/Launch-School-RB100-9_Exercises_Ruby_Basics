3180bf6eefe2a985cdd103acee97fbd51123820f
Michael080 <mrollin808@gmail.com>
2020-05-04 19:26:58 -0600

    Further exploration

--- 9_Exercises_Ruby-Basics/Debugging/8_Colorful-things.rb
+++ 9_Exercises_Ruby-Basics/Debugging/8_Colorful-things.rb
@@ -1,3 +1,5 @@
+puts "-----------
+myAnswer:"
 colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
 things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
 
@@ -6,7 +8,7 @@ things.shuffle!
 
 i = 0
 loop do
-  break if i >= colors.length - 1
+  break if i >= things.length
 
   if i == 0
     puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
@@ -18,3 +20,32 @@ loop do
 end
 # The following code throws an error. Find out what is
 # wrong and think about how you would fix it.
+
+
+# Further exploration
+# We might change our arrays in the future and we might not
+# always know which of them is shorter, so picking the shorter
+# one by hand is not the best solution. How can you change the
+# break condition, such that the loop always stops once we hit
+# the end of the shorter array?
+puts "-----------
+further exploration:"
+colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
+things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
+
+colors.shuffle!
+things.shuffle!
+
+i = 0
+loop do
+  # Break loop if the end of either array has been reached:
+  break if (i >= things.length) || (i >= colors.length)
+
+  if i == 0
+    puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
+  else
+    puts 'And a ' + colors[i] + ' ' + things[i] + '.'
+  end
+
+  i += 1
+end
